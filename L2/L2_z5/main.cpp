e71911f3686b39b4329e5b42a2dc4182d0bfbbb1
aleksy1221 <76480441+aleksy1221@users.noreply.github.com>
2021-03-17 10:13:43 +0100

    zadanie 5

--- L2/L2_z5/main.cpp
+++ L2/L2_z5/main.cpp
@@ -4,12 +4,11 @@ std::string find_and_replace(std::string inp, std::string s1, std::string s2){
     std::string odp="";
     size_t pos=0;
     size_t start=0;
-    while (pos!=std::string::npos) {
-        pos=inp.find(s1,pos+1);
-        odp=odp+inp.substr(start,pos)+s2;
+    while ((pos=inp.find(s1,pos+1))!=std::string::npos) {
+        odp=odp+inp.substr(start,pos-start)+s2;
         start=pos+s1.size();
-
     }
+    odp=odp+inp.substr(start,pos-start);
 
     return odp;
 }
