54d74396e611da1e90b8bdeb5307921fa9303095
aleksy1221 <76480441+aleksy1221@users.noreply.github.com>
2021-04-14 21:38:12 +0200

    Update main.cpp

--- L2/L2_z9/main.cpp
+++ L2/L2_z9/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
-using namespace std;
+struct pogoda
+{
+    std::string data;
+    std::string czas;
+    double vdzwieku;
+    double tdzwiekiem;
+    double cisnienie;
+    double temp;
+    double wilgotnosc;
+    int kompas;
+    double Vuw;
+    double vwiatr;
+    double kierunek;
+    double wysokosc;
+    double temp113;
+
+};
 
 int main()
 {
