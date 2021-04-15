4cdf0b43d967428d058b88c85cc2ddc5874b3f12
aleksy1221 <76480441+aleksy1221@users.noreply.github.com>
2021-04-15 21:55:45 +0200

    l4

--- L4/L4_z7/main.cpp
+++ L4/L4_z7/main.cpp
@@ -14,21 +14,32 @@ int main()
 //    res = std::regex_search(text, m, re); //zwróci wartość true i zwróci do m pierwszy pasujący podciąg czyli 1234
 //    std::cout<<res<<std::endl;
 //    std::cout<<m.str(0)<<std::endl;
-    std::cout << std::endl << "regex_search all" << std::endl;
-    std::string log(R"(
-          Speed: 366
-          Mass: 35
-          Speed: 378
-          Mass: 32
-          Speed: 400
-          Mass: 30)");
-    std::regex re(R"(Speed:\s(\d+))");
-    std::smatch sm;
-    while (regex_search(log, sm, re)) {
-          std::cout << sm[0] << std::endl; //Podciąg dla którego wzorzec jest w pełni dopasowany
-          std::cout << "Group 1: " << sm[1] << std::endl << std::endl; //grupa nr 1 (\d*)
+//    std::cout << std::endl << "regex_search all" << std::endl;
+//    std::string log(R"(
+//          Speed: 366
+//          Mass: 35
+//          Speed: 378
+//          Mass: 32
+//          Speed: 400
+//          Mass: 30)");
+//    std::regex re(R"(Speed:\s(\d+))");
+//    std::smatch sm;
+//    while (regex_search(log, sm, re)) {
+//          std::cout << sm[0] << std::endl; //Podciąg dla którego wzorzec jest w pełni dopasowany
+//          std::cout << "Group 1: " << sm[1] << std::endl << std::endl; //grupa nr 1 (\d*)
 
-          log = sm.suffix(); //przycięcie napisu do przeszukania
+//          log = sm.suffix(); //przycięcie napisu do przeszukania
+//    }
+    std::regex re(R"(P(O|Z)\s?\w{5})");
+    std::vector<std::string> lines = {"PO12345", "PO 12345", "PZ973ND", "WE20456"};
+    std::smatch m;
+    for(size_t i=0;i<lines.size();i++){
+        bool res = std::regex_match(lines, re);
+        if(res){
+            std::cout<<lines[i]<<std::endl;
+        }
     }
+
+
     return 0;
 }
